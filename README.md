c779496f7fa4b733b995e8834c8b93f20342cb7a
terrygonguet <terry@gonguet.com>
2017-12-06 17:00:52 +0000

    Did pretty stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # prettystuff
 An attempt to learn OpenGL, A-Frame (& three.js) and WebAssembly by making pretty things.
+
+* `git clone https://github.com/terrygonguet/prettystuff.git`
+* `cd prettystuff`
+* `npm i`
+* `npm start` 
--- static/script.js
+++ static/script.js
@@ -1,15 +1,53 @@
 AFRAME.registerComponent('rotator', {
-  schema: {},
+  schema: {type: 'number', default: 10},
   init: function () {
     this.angle = 0;
   },
   update: function () {},
   tick: function (time, delta) {
-    this.angle += 10 * delta / 1000;
+    this.angle += this.data * delta / 1000;
     this.angle >= 360 && (this.angle = 0);
     this.el.setAttribute("rotation", "35 " + this.angle + " 45");
   },
   remove: function () {},
   pause: function () {},
   play: function () {}
+});
+
+AFRAME.registerComponent('cloud', {
+  schema: {type: 'int', default: 700},
+  init: function () {
+    var dotGeometry = new THREE.Geometry();
+    for (var i = 0; i < this.data; i++) {
+      var dir = $V([
+        Math.random() * 2 - 1,
+        Math.random() * 2 - 1,
+        Math.random() * 2 - 1
+      ]).toUnitVector().x(Math.random() * 2);
+      dotGeometry.vertices.push(new THREE.Vector3(...dir.elements));
+    }
+    var dotMaterial = new THREE.PointsMaterial( { size: 2, sizeAttenuation: false } );
+    var dots = new THREE.Points( dotGeometry, dotMaterial );
+    this.el.setObject3D("points", dots);
+  },
+  update: function () {},
+  tick: function (time, delta) {
+  },
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+});
+
+AFRAME.registerComponent('wires', {
+  schema: {},
+  init: function () {
+    var geometry = new THREE.BoxGeometry(1,1,1);
+    var wireframe = new THREE.EdgesGeometry( geometry );
+    var lineMaterial = new THREE.LineBasicMaterial({
+      color: "#F00",
+      linewidth: 4
+    });
+    var mesh = new THREE.LineSegments(wireframe, lineMaterial);
+    this.el.setObject3D("mesh", mesh);
+  }
 });
